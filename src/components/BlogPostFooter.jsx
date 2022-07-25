2baddcf7a0e5c0f18405fe544191a4142ee260ef
Jay Mistry <jay@supertokens.com>
2022-07-25 12:22:40 +0530

    finish up sign up banner and move other footer content inside BlogPostFooter component

---
+++ src/components/BlogPostFooter.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import STBannerBackdrop from "../images/supertokens-logo-backdrop.png";
+
+const BlogPostFooter = ({ idSlug }) => {
+
+  return (
+    <footer id={`last_section_${idSlug}`}>
+      <b>Written by the Folks at <a href="https://supertokens.com">SuperTokens</a> — hope you enjoyed! We are always available on our Discord server. Join us if you have any questions or need any help.</b>
+      <div className="blog-post-sign-up-banner">
+        <img
+          src={STBannerBackdrop}
+          alt="Blog post sign up banner backdrop"
+          className="sign-up-banner-backdrop"
+        />
+
+        <div className="sign-up-banner-content">
+          <h2 className="sign-up-banner-content-title">Get started for free</h2>
+          <p className="sign-up-banner-content-paragraph">SuperTokens' free plan lets you go live and start signing up users (no credit card required)</p>
+          <div className="sign-up-banner-buttons-container">
+            <a
+              href="/auth"
+              className="sign-up-banner-content-cta"
+              id={`sign_up_${idSlug}`}
+            >Sign up</a>
+            <a
+              href="/discord"
+              className="sign-up-banner-content-discord-button"
+              id={`discord_${idSlug}`}
+            >
+              <img
+                src="/blog-footer-discord-logo.png"
+                alt="Join SuperTokens Discord channel"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+};
+
+export default BlogPostFooter;
