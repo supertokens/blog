44d005f61ba8066119524d512bebfab620db050b
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2023-10-02 15:12:33 +0530

    init social and email password login with reactjs

---
+++ content/how-to-set-up-social-and-email-password-login-with-reactjs/index.md
@@ -0,0 +1,10 @@
+---
+title: How to Set up Social and Email Password Login With ReactJS in 10 Minute
+date: "2022-02-09"
+description: " In this blog, we'll walk you through setting up an email-password authentication with popular social providers like Google, GitHub, and Apple using SuperTokens on a ReactJS application with ExpressJS as the backend."
+cover: "how-to-set-up-social-and-email-password-login-with-reactjs.png"
+category: "programming"
+author: "Rishabh Poddar"
+---
+
+hey yoyoyy
--- src/blog-details.js
+++ src/blog-details.js
@@ -1,18 +1,6 @@
 module.exports = [
   {
     fields: {
-      slug: "/how-to-set-up-social-and-email-password-login-with-reactjs",
-    },
-    frontmatter: {
-      title: "How to Set up Social and Email Password Login With ReactJS in 10 Minutes",
-      description: "In this blog, we'll walk you through setting up an email-password authentication with popular social providers like Google, GitHub, and Apple using SuperTokens on a ReactJS application with ExpressJS as the backend. ",
-      category: "programming",
-      date: "February 09, 2022",
-      cover: "how-to-set-up-social-and-email-password-login-with-reactjs.png"
-    }
-  },
-  {
-    fields: {
       slug: "/how-to-customise-supertokens-apis",
     },
     frontmatter: {
