21e5e950b6d3ea39ec255f483be22feaba350637
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2023-11-22 13:16:18 +0530

    add social links and responsive code

--- src/authors-details.js
+++ src/authors-details.js
@@ -1,14 +1,25 @@
+//TODO: data related to each author on the blog posts should be added incrementally here.
+
 module.exports = [
   {
     name: "Joel Coutinho",
     title: "Backend Developer @Supertokens",
-    image:"https://avatars.githubusercontent.com/u/87567452?v=4",
+    image: "https://avatars.githubusercontent.com/u/87567452?v=4",
     socials: [
       {
-        name: "Github",
-        url: "https://github.com/joel",
+        name: "github",
+        url: "https://github.com/jscyo",
+      },
+      {
+        name: "linkedin",
+        url: "https://github.com/jscyo",
+      },
+      {
+        name: "twitter",
+        url: "https://github.com/jscyo",
       },
     ],
-    description:"The one major downside of TOTP is that the secret key is stored on both the user’s device and the server. If either of these systems were to be compromised, a malicious actor would now be able to generate codes and have unfettered access to the user’s account."
+    description:
+      "The one major downside of TOTP is that the secret key is stored on both the user’s device and the server. If either of these systems were to be compromised, a malicious actor would now be able to generate codes and have unfettered access to the user’s account.",
   },
 ]
--- src/components/AuthorCardTop.jsx
+++ src/components/AuthorCardTop.jsx
@@ -20,6 +20,7 @@ export default function AuthorCard({ author }) {
       </p>
     )
   }
+  
   return (
     <div className="author-card-top-container">
       <img
